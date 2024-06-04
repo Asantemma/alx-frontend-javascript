d01648329155657426a7f2cca4c94f488e7ce53b
Emmanuel Asante <asantemma7@gmail.com>
2024-06-04 05:04:01 +0000

    fixed some bugs

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      const [photo, user] = results;
-      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-    })
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((res) => (
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`)
+    ))
     .catch(() => {
       console.log('Signup system offline');
     });
